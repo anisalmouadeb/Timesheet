f7e8e6d3633838f0002432d9067943648dda6bca
anisalmouadeb <anis.almouadeb@esprit.tn>
2021-11-14 16:05:22 +0100

    timesheet test add

--- src/test/java/tn/esprit/spring/TimesheetTest.java
+++ src/test/java/tn/esprit/spring/TimesheetTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.Role;
@@ -28,7 +29,7 @@ import org.apache.logging.log4j.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeTest {
+public class TimesheetTest {
 
 	@Autowired
 	ITimesheetService tss;
@@ -40,7 +41,7 @@ public class EmployeTest {
 	DepartementRepository dr;
 	@Autowired
 	IEmployeService employeService;
-	private static final Logger l = LogManager.getLogger(EmployeTest.class);
+	private static final Logger l = LogManager.getLogger(TimesheetTest.class);
 
 	
 
@@ -52,70 +53,70 @@ public class EmployeTest {
 	  }
 	 
 
-	/*
-	 * @Test
-	 *  public void affecterMissionADepartementTest() {
-	 * 
-	 * int idm = tss.ajouterMission(new Mission("missss", null)); if (idm > 0) {
-	 * 
-	 * l.info("mission added");
-	 * 
-	 * }
-	 * 
-	 * Departement departement = new Departement("Dept"); dr.save(departement);
-	 * l.info("departement created and added");
-	 * 
-	 * tss.affecterMissionADepartement(idm, departement.getId());
-	 * 
-	 * Departement dept = dr.findById(departement.getId()).get();
-	 * 
-	 * 
-	 * 
-	 * assertTrue("affectation mission a departement echouer",
-	 * mr.findById(idm).get().getDepartement().getId() == dept.getId());
-	 * 
-	 * if( mr.findById(idm).get().getDepartement().getId() == dept.getId()){
-	 * l.info("Mission found"); }else{ l.warn("warning check your method"); }
-	 * 
-	 * }
-	 * 
-	 */
-	/*
-	 * @Test public void findAllMissionByEmployeJPQLTest() throws ParseException
-	 * {
-	 * 
-	 * int employeId = employeService .ajouterEmploye(new Employe("anis", "mdb",
-	 * "anis@gmail.com", false, Role.TECHNICIEN));
-	 * l.info("employe added with id :" + employeId); int idm =
-	 * tss.ajouterMission(new Mission("M2SSION100", "Description"));
-	 * l.info("mission added with id :" + idm); int idm2 =
-	 * tss.ajouterMission(new Mission("M2SSION200", "Description2"));
-	 * l.info("mission added with id :" + idm2);
-	 * tss.affecterMissionADepartement(idm, 1);
-	 * l.info("mission affecter a departement");
-	 * tss.affecterMissionADepartement(idm2, 1);
-	 * l.info("mission affecter a departement"); SimpleDateFormat dateFormat =
-	 * new SimpleDateFormat("yyyy-MM-dd"); Date date1 =
-	 * dateFormat.parse("2022-03-23"); Date date2 =
-	 * dateFormat.parse("2022-03-28"); tss.ajouterTimesheet(idm, employeId,
-	 * date1, date2); l.info("timesheet added : employe " + employeId +
-	 * "mission " + idm); tss.validerTimesheet(idm, employeId, date1, date2, 1);
-	 * l.info("timesheet valider : employe " + employeId + "mission " + idm);
-	 * tss.ajouterTimesheet(idm2, employeId, date1, date2);
-	 * l.info("timesheet added : employe " + employeId + "mission " + idm2);
-	 * tss.validerTimesheet(idm2, employeId, date1, date2, 1);
-	 * l.info("timesheet valider : employe " + employeId + "mission " + idm2);
-	 * List<Mission> missions = tss.findAllMissionByEmployeJPQL(employeId);
-	 * assertThat(missions.size()).isEqualTo(2); if (missions.size() == 3) {
-	 * 
-	 * l.info("Missions founded"); } else { l.warn("warning check your method");
-	 * 
-	 * }
-	 * 
-	 * }
-	 */
+	
+	 @Test
+	   public void affecterMissionADepartementTest() {
+	  
+	  int idm = tss.ajouterMission(new Mission("missss", null)); if (idm > 0) {
+	  
+	  l.info("mission added");
+	  
+	  }
+	  
+	  Departement departement = new Departement("Dept"); dr.save(departement);
+	  l.info("departement created and added");
+	  
+	  tss.affecterMissionADepartement(idm, departement.getId());
+	  
+	  Departement dept = dr.findById(departement.getId()).get();
+	  
+	  
+	  
+	  assertTrue("affectation mission a departement echouer",
+	  mr.findById(idm).get().getDepartement().getId() == dept.getId());
+	  
+	  if( mr.findById(idm).get().getDepartement().getId() == dept.getId()){
+	  l.info("Mission found"); }else{ l.warn("warning check your method"); }
+	  
+	 }
+	 
+	
+	
+	 @Test public void findAllMissionByEmployeJPQLTest() throws ParseException
+	 {
+	 
+	 int employeId = employeService .ajouterEmploye(new Employe("anis", "mdb",
+	 "anis@gmail.com", false, Role.TECHNICIEN));
+	 l.info("employe added with id :" + employeId); int idm =
+	 tss.ajouterMission(new Mission("M2SSION100", "Description"));
+	 l.info("mission added with id :" + idm); int idm2 =
+	 tss.ajouterMission(new Mission("M2SSION200", "Description2"));
+	 l.info("mission added with id :" + idm2);
+	 tss.affecterMissionADepartement(idm, 1);
+	 l.info("mission affecter a departement");
+	tss.affecterMissionADepartement(idm2, 1);
+	l.info("mission affecter a departement"); SimpleDateFormat dateFormat =
+	 new SimpleDateFormat("yyyy-MM-dd"); Date date1 =
+	 dateFormat.parse("2022-03-23"); Date date2 =
+	 dateFormat.parse("2022-03-28"); tss.ajouterTimesheet(idm, employeId,
+	 date1, date2); l.info("timesheet added : employe " + employeId +
+	 "mission " + idm); tss.validerTimesheet(idm, employeId, date1, date2, 1);
+	 l.info("timesheet valider : employe " + employeId + "mission " + idm);
+	 tss.ajouterTimesheet(idm2, employeId, date1, date2);
+	 l.info("timesheet added : employe " + employeId + "mission " + idm2);
+	 tss.validerTimesheet(idm2, employeId, date1, date2, 1);
+	 l.info("timesheet valider : employe " + employeId + "mission " + idm2);
+	 List<Mission> missions = tss.findAllMissionByEmployeJPQL(employeId);
+	 assertThat(missions.size()).isEqualTo(2); if (missions.size() == 3) {
+	 
+	 l.info("Missions founded"); } else { l.warn("warning check your method");
+	 
+	 }
+	 
+	 }
+	
 
-	/*@Test
+	@Test
 	public void getAllEmployeByMissionTest() throws ParseException {
 
 		int employeId = employeService
@@ -148,5 +149,5 @@ public class EmployeTest {
 
 	}
 
-}*/
+}
 }
